3aa9d31f4a7359970a67dcb044d3bc33368b95b4
Meera Vasudevan <meera.vasudevan@rea-group.com>
2018-08-15 14:16:26 +1000

    Implement onClick method for buttons
    - To check, we display the cell id using Toasts

--- app/src/main/java/com/example/meeravasudevan/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/meeravasudevan/tictactoe/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.meeravasudevan.tictactoe
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +12,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun getCellId(view: View) {
+        val selectedButton = view as Button
+
+        val cellId = when(selectedButton.id) {
+            R.id.Button_00 -> 1
+            R.id.Button_01 -> 2
+            R.id.Button_02 -> 3
+            R.id.Button_10 -> 4
+            R.id.Button_11 -> 5
+            R.id.Button_12 -> 6
+            R.id.Button_20 -> 7
+            R.id.Button_21 -> 8
+            R.id.Button_22 -> 9
+            else -> 0
+        }
+
+        Toast.makeText(this, "Cell ID: $cellId", Toast.LENGTH_SHORT).show()
+    }
+
 }
